e784e77ea72609790ec4189603db0d5cdf4a8a7a
Lily9906 <bcs21090014@student.uts.edu.my>
2022-04-08 11:55:04 +0800

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LilyLohTzeEng_BCS21090014
+{
+    class Student : ObjectOrientedProgrammingCourse
+    {
+        StudentDetail st = new StudentDetail("Lily", 12, 9906, 50);
+        StudentDetail st2 = new StudentDetail("James", 13, 9907, 60);
+        StudentDetail st3 = new StudentDetail("April", 14, 9908, 70);
+
+    }
+
+    internal class StudentDetail
+    {
+    }
+}
+
+
+
+
+public class Student 
+{
+
+}
---
+++ ConsoleApp6/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LilyLohTzeEng_BCS21090014
+{
+    class ObjectOrientedProgrammingCourse
+    {
+        public StudentDetail(string studentname, string studentid, string identificationcard, decimal mark)
+        {
+   
+            StudentName = studentname;
+            StudentId = studentid;
+            IdentificationCard = identificationcard;
+            Mark = mark;
+        }
+        public string StudentName { get; set; }
+        public string StudentId { get; set; }
+        public string IdentificationCard { get; set; }
+        public decimal Mark { get; set; }
+
+    }
+}
+
